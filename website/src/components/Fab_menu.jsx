15e03d3c65c13e83b1684a7ef909b6bdb2647b7f
YaldaGhaemiZadeh <yalda.ghaemi.zadeh@gmail.com>
2025-02-24 21:24:27 +0330

    fab menu animated

--- website/src/components/Fab_menu.jsx
+++ website/src/components/Fab_menu.jsx
@@ -12,20 +12,18 @@ const Fabmenu = () => {
     const toggle = () =>{
             setBtn1(prevClass => 
             prevClass === 'btn1-hide' ? 'btn1-show' : 'btn1-hide' );
+
             setBtn2(prevClass => 
                 prevClass === 'btn2-hide' ? 'btn2-show' : 'btn2-hide' );
-        
     }
 
 
     return ( 
-        <div className="fixed bottom-[20px] right-[20px] z-40">
-            <div className="flex flex-col relative">
-                <button id="btn2" className={`custom-fab z-10 ${btn2}`}> <Sun /> </button>
-                <button id="btn1" className={`custom-fab z-20 ${btn1}`}> <Mag_glass /> </button>
-                <button id="btn0" className="bg-[#001FE6] w-[70px] h-[70px] rounded-full  hover:bg-[#008eff] pl-2.5 pb-1 z-30" onClick={toggle}>
+        <div className="fixed bottom-[20px] right-[20px] z-40 w-[300] h-[400]">
+                <button id="btn2" className={`custom-fab  ${btn2}`}> <Sun /> </button>
+                <button id="btn1" className={`custom-fab  ${btn1}`}> <Mag_glass /> </button>
+                <button id="btn0" className="relative bg-[#001FE6] w-[70px] h-[70px] rounded-full  hover:bg-[#008eff] pl-2.5 pb-1" onClick={toggle}>
                 <Hamburger /> </button>
-            </div>
         </div>
      );
 }
