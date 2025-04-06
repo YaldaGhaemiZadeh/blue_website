d8a3efe48718c815d621bf1a40a11cb9a2d05065
YaldaGhaemiZadeh <yalda.ghaemi.zadeh@gmail.com>
2025-04-06 23:22:00 +0330

    Update{slider automatized, item end-point style updated}

--- website/src/components/Shop/Item001.jsx
+++ website/src/components/Shop/Item001.jsx
@@ -3,18 +3,20 @@ const Product1 = () => {
     const title = 'item number one model x serie y'
     const properties = ['size', 'weight']
     const price = 2000
-
+    // bg-cyan-950
     return ( 
     <div className="flex justify-center mt-10">   
         <div className="flex flex-row w-[1020px] h-[440px] rounded-2xl bg-[#313131] ">
-            <div className="w-[500px] h-[440px] bg-cyan-500">
-                <div className="bg-cyan-950 pt-4 pl-5 h-28 font-sans text-[30px] ">{title}</div>
+            <div className="w-[500px] h-[440px] ">
+                <div className="pt-5 pl-5 h-28 font-sans text-[30px] font-semibold">{title}</div>
                 <div className="w-[500px] h-60 bg-blue-600">{properties}</div>
+
                 <div className="flex flex-row bg-cyan-300 w-[500px] h-[88px]">
-                    <p>{price}</p>
-                    <div className="w-80 h-2 bg-black"></div>
-                    <button>add to cart</button>
-                </div>
+                    <p className="font-sans text-[30px] font-semibold pt-5 pl-5">{price}</p>
+                    <div className="w-64 h-2 bg-black"></div>
+                    <button className="h-12 mt-5 p-3 font-sans text-[20px] font-semibold border border-cyan-500 border-solid">add to cart</button>
+                </div> 
+
             </div>
 
             <div className="w-[520px] h-[440px] bg-cyan-800 ">pic</div>
--- website/src/components/Slider/Swiper_slider.jsx
+++ website/src/components/Slider/Swiper_slider.jsx
@@ -1,11 +1,16 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import Blog from './Blog'
 
 const Swiperslider = () => {
     return ( 
 
-    <Swiper spaceBetween={1} slidesPerView={3} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
+    <Swiper centeredSlides={false}
+    pagination={{ clickable: true, }}
+    navigation={true}
+    modules={[Autoplay, Pagination, Navigation]}
+    autoplay={{ delay: 2500, disableOnInteraction: false, }} spaceBetween={1} slidesPerView={3} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
         <SwiperSlide> <Blog /> </SwiperSlide>
         <SwiperSlide> <Blog /> </SwiperSlide>
         <SwiperSlide> <Blog /> </SwiperSlide>
