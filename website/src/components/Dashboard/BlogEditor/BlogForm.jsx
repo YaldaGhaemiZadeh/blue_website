b3b2891c5214a6b9340b6e3b02ad07b64146530b
yalda ghaemi <yalda.ghaemi.zadeh@gmail.com>
2025-07-31 14:58:13 -0700

    UI { blog editor component's ui improved }

--- website/src/components/Dashboard/BlogEditor/BlogForm.jsx
+++ website/src/components/Dashboard/BlogEditor/BlogForm.jsx
@@ -1,10 +1,10 @@
 const BlogForm = () => {
     return ( 
-        <form action="" className="w-[750px] h-[500px] flex flex-col px-6 py-5 bg-[#313131] shadow-xl rounded-3xl">
+        <form action="" className="w-[750px] h-[500px] flex flex-col gap-3 px-6 py-5 bg-[#313131] shadow-xl rounded-3xl">
             <input type="text" placeholder="Title..." className="w-[700px] h-20 rounded-xl text-[50px] italic p-6" />
                 
-                <div className="w-[700px] h-[50px] bg-[#3b3b3b] mt-3">
-                 <select name="" id="">
+                <div className="w-[700px] h-[60px] p-1 flex gap-2 justify-between text-3xl bold italic">
+                 <select name="" id="" className="w-[170px] h-[50px] rounded-lg p-1 bg-[#3b3b3b] text-[#9ca3af]">
                    <option value="">
                     Phone
                    </option>
@@ -16,7 +16,12 @@ const BlogForm = () => {
                    </option>
                  </select>
 
-                </div>
+          <button className="w-[120px] h-[50px] bg-green-700 rounded-lg">Post</button>
+          <button className="w-[120px] h-[50px] bg-red-900 rounded-lg">Delete</button>
+          <button className="w-[120px] h-[50px] bg-[#3b3b3b] rounded-lg">???</button>
+          <button className="w-[120px] h-[50px] bg-[#3b3b3b] rounded-lg">???</button>
+               </div>
+            <textarea className="h-[300px] rounded-xl p-5 text-2xl"></textarea>
         </form>
      );
 }
